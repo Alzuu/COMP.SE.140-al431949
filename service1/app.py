ee4a9dc78de5f758a34940be6e438a58deb4922a
Allan Li <allan0499@gmail.com>
2023-09-26 18:20:17 +0300

    Add comments to code

--- service1/app.py
+++ service1/app.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from pathlib import Path
 
 
+# Send an HTTP POST request to a given URL with text data
 def send_request(url, text, headers):
     try:
         requests.post(url, json={"data": text}, headers=headers)
@@ -14,6 +15,7 @@ def send_request(url, text, headers):
         return f"Error {str(e)}"
 
 
+# Write text data to a log file
 def write_log(file, text):
     if not text:
         with open(Path(file), "w") as log_file:
@@ -23,10 +25,12 @@ def write_log(file, text):
             log_file.write(f"{text}\n")
 
 
+# Exit the application
 def exit_app(url, headers):
     print("exit service1")
     stop = "STOP"
     try:
+        # Send a final request with "STOP" to the URL and write it to the log file
         send_request(url, stop, headers)
         write_log(Path(file_path), stop)
     except Exception as e:
@@ -39,14 +43,18 @@ if __name__ == "__main__":
     file_path = "../logs/service1.log"
     dir_path = "../logs"
 
+    # Create the log directory if it doesn't exist
     if not Path(dir_path).exists():
         Path(dir_path).mkdir(parents=True, exist_ok=True)
 
+    # Retrieve environment variables for the target host and port
     target_host = os.getenv("TARGET_HOST")
     target_port = os.getenv("TARGET_PORT")
 
+    # Combine the host and port to form the target address
     target = f"{target_host}:{target_port}"
 
+    # Create the URL for HTTP requests using the target address
     url = f"http://{target}/"
 
     headers = {
@@ -54,13 +62,17 @@ if __name__ == "__main__":
         "Accept": "application/json, text/plain",
     }
 
+    # Initialize the log file with an empty line
     write_log(Path(file_path), "")
 
+    # Send 20 requests with a pause of 2 seconds between each request
     for i in range(1, 21):
         current_datetime = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
         text = f"{i} {current_datetime} {target}"
         result = send_request(url, text, headers)
+        # Write the result to the log file
         write_log(Path(file_path), result)
         time.sleep(2)
 
+    # Exit the application
     exit_app(url, headers)
--- service2/src/index.js
+++ service2/src/index.js
@@ -1,27 +1,38 @@
 const express = require('express')
 const fs = require('fs').promises
 
+// Create an Express app and specify the port
 const app = express()
 const port = 8000
 
+// Define the log file path
 const logFile = '../logs/service2.log'
 
+// Parse incoming JSON requests
 app.use(express.json())
 
+// Handle POST requests to the root path
 app.post('/', async (req, res) => {
   try {
+    // Check if the request data is not 'STOP'
     if (req.body && req.body.data !== 'STOP') {
+      // Create a log entry with data and client info
       const data = `${req.body.data} ${req.socket.remoteAddress}:${req.socket.remotePort}\n`
+
+      // Send the log data as a response and save it to the log file
       res.send(data)
       await fs.writeFile(logFile, data, { flag: 'a+' })
     }
   } catch (error) {
+    // Log errors
     console.log(error)
   }
 })
 
+// Start the server after a 2-second delay
 setTimeout(() => {
   app.listen(port, () => {
+    // Log server start and clear the log file
     console.log(`Service2 listening on port ${port}`)
     fs.writeFile(logFile, '', { flag: 'w' })
   })
